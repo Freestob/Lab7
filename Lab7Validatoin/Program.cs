83fbd411ad99fd1b79541e7de301f6624e9b5cdf
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-23 15:26:20 -0400

    Got the basics of email to work

--- Lab7Validatoin/Program.cs
+++ Lab7Validatoin/Program.cs
@@ -15,14 +15,19 @@ namespace Lab7Validatoin
             Console.WriteLine("This will validate different kinds of input");
 
             var userName = GetUserName();
-            Console.WriteLine(userName);
+            Console.WriteLine("Hello " + userName);
             Console.ReadKey();
+            var email = GetUserEmail();
+            Console.WriteLine(email);
+            Console.ReadLine();
+
+
         }
         static string GetUserName()
         {
-            Console.WriteLine("Please enter in your name");
+            Console.WriteLine("Please enter in your first name.");
             string userName = Console.ReadLine();
-            Regex nameRegex = new Regex(@"[a-z]\D");
+            Regex nameRegex = new Regex(@"^[A-Z][a-z]\D{0,30}$");
             if (nameRegex.IsMatch(userName))
             { 
                 return userName;
@@ -30,6 +35,21 @@ namespace Lab7Validatoin
             else
             {
                 return GetUserName();
+            }
+
+        }
+        static string GetUserEmail()
+        {
+            Console.WriteLine("Please enter your email");
+            string email = Console.ReadLine();
+            Regex emailRegex = new Regex(@"^(\w{5,30})@(\w{5,10}).(\w{2,3}$)");
+            if(emailRegex.IsMatch(email))
+            {
+                return email;
+            }
+            else
+            {
+                return GetUserEmail();
             }
         }
     }
