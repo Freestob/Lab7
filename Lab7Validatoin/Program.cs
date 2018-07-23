925b9dd53860cc3c297d944a90e102633715bc46
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-23 14:45:35 -0400

    Added beginning of username validation

---
+++ Lab7Validatoin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Lab7Validatoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will validate different kinds of input");
+
+            var userName = GetUserName();
+            Console.WriteLine(userName);
+            Console.ReadKey();
+        }
+        static string GetUserName()
+        {
+            Console.WriteLine("Please enter in your name");
+            string userName = Console.ReadLine();
+            Regex nameRegex = new Regex(@"[a-z]\D");
+            if (nameRegex.IsMatch(userName))
+            { 
+                return userName;
+            }
+            else
+            {
+                return GetUserName();
+            }
+        }
+    }
+}
